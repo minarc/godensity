24868f5d1d8a7b46c1037ba26d911a56f9f2e9d2
mowinckel <ragnhildmowinckel@gmail.com>
2020-07-31 15:45:36 +0900

    first commit

---
+++ godensity_test.go
@@ -0,0 +1,98 @@
+package godensity
+
+import (
+	"log"
+	"net/http"
+	"sort"
+	"strings"
+	"testing"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func TestDensity(t *testing.T) {
+
+	urls := []string{
+		"https://news.joins.com/article/23707620?cloc=joongang-home-toptype1basic",
+		"https://news.v.daum.net/v/20200216130927758#none",
+		"https://namu.wiki/w/설리(배우)",
+		"https://www.yna.co.kr/view/AKR20200217067251001?section=politics/national-assembly",
+		"http://www.hani.co.kr/arti/politics/bluehouse/928518.html",
+		"https://gall.dcinside.com/board/lists/?id=baseball_new8",
+		"https://blog.bobthedeveloper.io/happy-new-year-from-bob-86b018fd134a",
+		"http://www.hani.co.kr/arti/politics/bluehouse/928518.html",
+		"https://m.blog.naver.com/PostView.nhn?blogId=forsun55&logNo=220923292175&proxyReferer=https%3A%2F%2Fwww.google.com%2F",
+		"https://www.dogdrip.net/index.php?mid=computer&category=180739712&document_srl=246186396&page=1",
+		"http://www.donga.com/news/article/all/20200221/99817201/2",
+		"https://gall.dcinside.com/board/view/?id=hit&no=15657&page=1",
+		"http://magazine.hankyung.com/business/apps/news?mode=sub_view&nkey=2011022200795000041",
+	}
+
+	for _, url := range urls {
+
+		if res, err := http.Get(url); err != nil {
+			log.Fatal(err)
+		} else {
+			if doc, err := goquery.NewDocumentFromResponse(res); err != nil {
+				log.Fatal(err)
+			} else {
+				body := doc.Find("body")
+				Filtering(body)
+				DiveIntoDOM(body)
+			}
+		}
+
+		sort.Slice(array, func(i, j int) bool {
+			return array[i].densitySum > array[j].densitySum
+		})
+
+		var candidates []Node
+		var threashold float32
+
+		log.Println("------------")
+		log.Println(url)
+
+		for _, element := range array {
+
+			// log.Println(goquery.NodeName(element.goqueryNode), element.density, element.densitySum)
+			if strings.Contains(goquery.NodeName(element.goqueryNode), "body") {
+				threashold = element.density
+				break
+			}
+
+			candidates = append(candidates, element)
+		}
+
+		if len(candidates) == 0 {
+			candidates = append(candidates, array[1])
+		}
+
+		// for _, c := range array {
+		// fmt.Println(c.tag, c.density, c.densitySum)
+		// }
+
+		// visited := make(map[*Node]bool)
+
+		// log.Println(threashold)
+		// log.Println(candidates)
+
+		for _, element := range candidates {
+			var cursor *Node = &element
+			// var slower *Node = cursor
+			if element.density < threashold {
+				continue
+			}
+			log.Println(cursor)
+			break
+			// for cursor.next != nil && cursor.density >= threashold {
+			// 	slower = cursor
+			// 	cursor = cursor.next
+			// }
+
+			// fmt.Println(slower.text)
+			// fmt.Println("-------------")
+		}
+		array = array[:0]
+	}
+
+}
---
+++ type.go
@@ -0,0 +1,22 @@
+package godensity
+
+import "github.com/PuerkitoBio/goquery"
+
+type Result struct {
+	root *Node
+	heap *Heap
+}
+
+type Node struct {
+	goqueryNode *goquery.Selection
+	density     float32
+	densitySum  float32
+	images      []string
+	T           float32
+	text        string
+	next        *Node
+}
+
+type Heap struct {
+	nodes []Node
+}
