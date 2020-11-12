50c60c95e703c5ba78cfbd8f0257a5a948882164
arcmin <11865340+minarc@users.noreply.github.com>
2020-11-13 02:23:43 +0900

    UPDATE :: don't know

--- godensity.go
+++ godensity.go
@@ -1,6 +1,11 @@
 package godensity
 
 import (
+	"bytes"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
 	"regexp"
 	"strings"
 
@@ -23,17 +28,64 @@ func DensitySum() {
 
 }
 
-func DiveIntoDOM(me *goquery.Selection) *Node {
+func IsGIF(src *url.URL, currentURL *url.URL) bool {
+	target := src.String()
+
+	if !src.IsAbs() {
+		target = currentURL.ResolveReference(src).String()
+	}
+
+	res, err := http.Get(target)
+
+	if err != nil {
+		log.Println(src, currentURL, target)
+		return false
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	// GIF magic number
+	// 47 49 46 38 37 61
+	// 47 49 46 38 39 61
+	if bytes.Compare(body[:4], []byte{71, 73, 70, 56}) == 1 {
+		log.Println("GIF!", target)
+		return true
+	}
+	return false
+}
+
+func DiveIntoDOM(me *goquery.Selection, domain string) *Node {
 	if me.Children().Length() == 0 {
 		re := regexp.MustCompile(`\s+`)
 		characters := re.ReplaceAllString(strings.TrimSpace(me.Text()), " ")
-		density := float32(len(characters))
+		// density := float32(len(characters))
+		var density float32 = 0
+
+		if goquery.NodeName(me) == "img" {
+			if src, exist := me.Attr("src"); !exist {
+				log.Println("error")
+			} else {
+				s, _ := url.Parse(src)
+				d, _ := url.Parse(domain)
+				IsGIF(s, d)
+				density = 1
+			}
+		}
 
 		return &Node{
 			goqueryNode: me,
 			density:     density,
 			densitySum:  0,
 			images: me.Filter("img").Map(func(_ int, s *goquery.Selection) string {
+				src, _ := s.Attr("src")
+				return src
+			}),
+			videos: me.Filter("video > source").Map(func(_ int, s *goquery.Selection) string {
 				src, _ := s.Attr("src")
 				return src
 			}),
@@ -49,7 +101,7 @@ func DiveIntoDOM(me *goquery.Selection) *Node {
 	var T float32
 
 	me.Children().Each(func(_ int, c *goquery.Selection) {
-		child := DiveIntoDOM(c)
+		child := DiveIntoDOM(c, domain)
 
 		densitySum += child.density
 		T += child.T
@@ -65,10 +117,10 @@ func DiveIntoDOM(me *goquery.Selection) *Node {
 	// fmt.Println(len(me.Children().Text()))
 	re := regexp.MustCompile(`\s+`)
 
-	var C float32 = float32(len(re.ReplaceAllString(strings.TrimSpace(me.Text()), " ")))
+	// var C float32 = float32(len(re.ReplaceAllString(strings.TrimSpace(me.Text()), " ")))
 	var LT float32 = float32(me.Find("a").Length())
 
-	var LC float32 = float32(len(re.ReplaceAllString(strings.TrimSpace(me.Find("a").Text()), " ")))
+	// var LC float32 = float32(len(re.ReplaceAllString(strings.TrimSpace(me.Find("a").Text()), " ")))
 	// var nLC float32 = C - LC
 	// var nLC float32 = C - LC
 	if T-LT <= 0 {
@@ -76,18 +128,34 @@ func DiveIntoDOM(me *goquery.Selection) *Node {
 		LT = 0
 	}
 
-	var I float32 = float32(me.Find("img").Length())
+	// var I float32 = float32(me.Find("img").Length())
 	var V float32 = float32(me.Find("video").Length())
-	if I == 0 {
-		I = 1
+	var G float32 = 0
+	var GIFs []string
+
+	me.Find("img").Each(func(i int, s *goquery.Selection) {
+		src, _ := s.Attr("src")
+		if strings.Contains(src, ".gif") && !strings.Contains(src, "icon") {
+			G++
+			GIFs = append(GIFs, src)
+		}
+	})
+
+	// if I == 0 {
+	// 	I = 1
+	// }
+	if G == 0 {
+		G = 1
 	}
 	if V == 0 {
 		V = 1
 	}
+
 	// fmt.Println(class, (C-LC)/(T-LT), I, C, LC, T, LT, re.ReplaceAllString(strings.TrimSpace(me.Text()), " "))
 	// fmt.Println(class, (C-LC)/(T-LT), I, C, LC, T, LT)
-	var density float32 = (C - LC) / (T - LT)
-	// densitySum *= I + V
+	// var density float32 = (C-LC)/(T-LT) + (GIFs * V)
+	var density float32 = (G * V)
+	densitySum *= G * V
 
 	// density := (C / T) * float32(math.Log10(float64((C/LC)*(T/LT)))/math.Log10(math.Log(float64(((C/nLC)*LC)+LC+math.E))))
 
@@ -95,7 +163,8 @@ func DiveIntoDOM(me *goquery.Selection) *Node {
 		goqueryNode: me,
 		density:     density,
 		densitySum:  densitySum,
-		images: me.Find("img").Map(func(_ int, s *goquery.Selection) string {
+		images:      GIFs,
+		videos: me.Find("video > source").Map(func(_ int, s *goquery.Selection) string {
 			src, _ := s.Attr("src")
 			return src
 		}),
--- godensity_test.go
+++ godensity_test.go
@@ -1,6 +1,7 @@
 package godensity
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"sort"
@@ -13,19 +14,22 @@ import (
 func TestDensity(t *testing.T) {
 
 	urls := []string{
-		"https://news.joins.com/article/23707620?cloc=joongang-home-toptype1basic",
-		"https://news.v.daum.net/v/20200216130927758#none",
-		"https://namu.wiki/w/설리(배우)",
-		"https://www.yna.co.kr/view/AKR20200217067251001?section=politics/national-assembly",
-		"http://www.hani.co.kr/arti/politics/bluehouse/928518.html",
-		"https://gall.dcinside.com/board/lists/?id=baseball_new8",
-		"https://blog.bobthedeveloper.io/happy-new-year-from-bob-86b018fd134a",
-		"http://www.hani.co.kr/arti/politics/bluehouse/928518.html",
-		"https://m.blog.naver.com/PostView.nhn?blogId=forsun55&logNo=220923292175&proxyReferer=https%3A%2F%2Fwww.google.com%2F",
-		"https://www.dogdrip.net/index.php?mid=computer&category=180739712&document_srl=246186396&page=1",
-		"http://www.donga.com/news/article/all/20200221/99817201/2",
-		"https://gall.dcinside.com/board/view/?id=hit&no=15657&page=1",
-		"http://magazine.hankyung.com/business/apps/news?mode=sub_view&nkey=2011022200795000041",
+		// "https://gifsf.com/star/4264545",
+		"https://www.heye.kr/board/index.html?id=idol&no=57606",
+
+		// "https://news.joins.com/article/23707620?cloc=joongang-home-toptype1basic",
+		// "https://news.v.daum.net/v/20200216130927758#none",
+		// "https://namu.wiki/w/설리(배우)",
+		// "https://www.yna.co.kr/view/AKR20200217067251001?section=politics/national-assembly",
+		// "http://www.hani.co.kr/arti/politics/bluehouse/928518.html",
+		// "https://gall.dcinside.com/board/lists/?id=baseball_new8",
+		// "https://blog.bobthedeveloper.io/happy-new-year-from-bob-86b018fd134a",
+		// "http://www.hani.co.kr/arti/politics/bluehouse/928518.html",
+		// "https://m.blog.naver.com/PostView.nhn?blogId=forsun55&logNo=220923292175&proxyReferer=https%3A%2F%2Fwww.google.com%2F",
+		// "https://www.dogdrip.net/index.php?mid=computer&category=180739712&document_srl=246186396&page=1",
+		// "http://www.donga.com/news/article/all/20200221/99817201/2",
+		// "https://gall.dcinside.com/board/view/?id=hit&no=15657&page=1",
+		// "http://magazine.hankyung.com/business/apps/news?mode=sub_view&nkey=2011022200795000041",
 	}
 
 	for _, url := range urls {
@@ -38,7 +42,7 @@ func TestDensity(t *testing.T) {
 			} else {
 				body := doc.Find("body")
 				Filtering(body)
-				DiveIntoDOM(body)
+				DiveIntoDOM(body, url)
 			}
 		}
 
@@ -54,7 +58,9 @@ func TestDensity(t *testing.T) {
 
 		for _, element := range array {
 
-			// log.Println(goquery.NodeName(element.goqueryNode), element.density, element.densitySum)
+			class, _ := element.goqueryNode.Attr("id")
+			log.Println(goquery.NodeName(element.goqueryNode), element.density, element.densitySum, class)
+
 			if strings.Contains(goquery.NodeName(element.goqueryNode), "body") {
 				threashold = element.density
 				break
@@ -67,9 +73,12 @@ func TestDensity(t *testing.T) {
 			candidates = append(candidates, array[1])
 		}
 
-		// for _, c := range array {
-		// fmt.Println(c.tag, c.density, c.densitySum)
-		// }
+		array = array[:10]
+		for _, c := range array {
+			id, _ := c.goqueryNode.Attr("id")
+			class, _ := c.goqueryNode.Attr("class")
+			fmt.Println(goquery.NodeName(c.goqueryNode), c.density, c.densitySum, id, class)
+		}
 
 		// visited := make(map[*Node]bool)
 
