189e34a7924d44a28790e394c1d7e6ad70fde1e5
arcmin <11865340+minarc@users.noreply.github.com>
2024-09-07 16:24:30 +0900

    ♻️ (filter.go): remove filter.go file and move Filtering function to godensity.go for better organization ✨ (godensity.go): add Filtering function to godensity.go to simplify DOM analysis and improve code structure

--- godensity.go
+++ godensity.go
@@ -36,6 +36,20 @@ func CalculateDensitySum(node *Node) float32 {
 	return node.density + node.densitySum
 }
 
+// Filtering removes unnecessary HTML elements from the page's body to simplify the DOM analysis.
+func Filtering(body *goquery.Selection) {
+	// Remove common interactive and irrelevant elements from the DOM
+	toRemove := []string{
+		"script", "style", "br", "input", "button", "textarea", "label", "form", "iframe",
+		"header", "nav", ".comment", "footer", ".footer", "#footer",
+	}
+
+	// Iterate over the list and remove each element
+	for _, selector := range toRemove {
+		body.Find(selector).Remove()
+	}
+}
+
 // IsGIF checks if the provided URL points to a GIF image by examining its content
 func IsGIF(src *url.URL, currentURL *url.URL) bool {
 	target := src.String()
